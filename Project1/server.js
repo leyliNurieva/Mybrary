9535dc61dbd3afaa4d3b4b3288d304b41e902a48
leyliNurieva <55515907+leyliNurieva@users.noreply.github.com>
2019-12-16 23:30:15 +0500

    Initial Author Routes

--- Project1/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', )
-
