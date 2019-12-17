2243b5fc60bc45ab15754d5d1e6e6860ff9d672f
leyliNurieva <55515907+leyliNurieva@users.noreply.github.com>
2019-12-17 22:25:36 +0500

    Initial Book Routes

---
+++ routes/books.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+//All Books Route
+router.get('/', async (req, res) => {
+    res.send('All Books')    
+})
+
+
+// New Book Route
+router.get('/new', async (req, res) => {
+    try{
+        const authors = await authors.find({})
+        const book = new Book()
+        res.render('books/new', {
+            authors: authors,
+            book: book
+        })
+    } catch{
+        res.redirect('/books')
+    }
+})
+
+//Create Book Route
+router.post('/', async (req, res) => {
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        publishDate: new Date(req.body.publishDate),
+        pageCount: req.body.pageCount,
+        description: req.body.description
+    })
+})
+
+
+module.exports = router
